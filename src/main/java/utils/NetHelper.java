decd33c9e57b8fd6ffd6e9763ec03c9e91c5c1c0
Силаев Александр Николаевич <a.silaev@bssys.com>
2018-03-13 18:31:46 +0300

    add -Xmx for launch maven-surefire-plugin

--- src/main/java/utils/NetHelper.java
+++ src/main/java/utils/NetHelper.java
@@ -1,11 +1,11 @@
 package utils;
-import com.jayway.restassured.RestAssured;
 
+import com.jayway.restassured.RestAssured;
 import user.User;
 
 public class NetHelper {
 
     public static void sendUserCredentials(User user) {
-        RestAssured.given().when().get("http://test.levx.ru/rest.php?uname=" + user.getName() + "&pass=" + user.getPassword() + "").then().statusCode(200);
+        RestAssured.given().when().get("http://test.levx.ru/rest.php?uname=" + user.getName() + "&pass=" + user.getPassword()).then().statusCode(200);
     }
 }
